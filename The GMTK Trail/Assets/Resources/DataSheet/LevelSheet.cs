f68af80d0b6623fdfe17d562708559fd78ec971d
RandolphLIN <2933089932@qq.com>
2021-06-13 02:00:06 +0800

    111

--- The GMTK Trail/Assets/Resources/DataSheet/LevelSheet.cs
+++ The GMTK Trail/Assets/Resources/DataSheet/LevelSheet.cs
@@ -5,21 +5,27 @@ using System;
 using geniikw.DataSheetLab;
 
 
-    [Serializable]
-    public class LevelParameter
+[Serializable]
+public class LevelParameter
 {
-        //ID
-        public int ID;
+    //ID
+    public int ID;
 
-        //名字
-        public string name;
+    //名字
+    public string name;
 
     //关卡内动物ID(AnimalSheetID)
     public List<int> Animals;
-    }
 
-    /// <summary>
-    /// sheet class name must be same with file name.
-    /// </summary>
-    [CreateAssetMenu]
-    public class LevelSheet : Sheet<LevelParameter> { }
+    //下一关 场景名
+    public string NameOfNextScene;
+
+    //这一关 这一关场景名
+    public string NameOfThisScene;
+}
+
+/// <summary>
+/// sheet class name must be same with file name.
+/// </summary>
+[CreateAssetMenu]
+public class LevelSheet : Sheet<LevelParameter> { }
