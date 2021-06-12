6368b4eaab8ed2008d8d970d6be1bb3fc0f811be
qinyishen <qinyishen163@126.com>
2021-06-13 00:31:35 +0800

    SheetManager

--- The GMTK Trail/Assets/Resources/DataSheet/AnimalDataBaseSheet.cs
+++ The GMTK Trail/Assets/Resources/DataSheet/AnimalDataBaseSheet.cs
@@ -5,11 +5,11 @@ using System;
 using geniikw.DataSheetLab;
 
 
-    [Serializable]
-    public class AnimalDataBaseParameter
+[Serializable]
+public class AnimalDataBaseParameter
 {
-        //ID
-        public int ID;
+    //ID
+    public int ID;
 
     //AnimalID
     public int AnimalID;
@@ -17,18 +17,40 @@ using geniikw.DataSheetLab;
     //所属关卡
     public int Level;
 
-        //名字
-        public string name;
-        
-        //物种，母语
-        public AnimalType type;
-        
-        //二语言栏
-        public List<AnimalType> slots;
+    //名字
+    public string name;
+
+    //物种，母语
+    public AnimalType type;
+
+    //二语言栏
+    // public List<AnimalType> slots;
+
+    //新二语言栏 by Yishen
+    public Queue<LanguageParameter> newSlots;
+    public readonly int capacity;
+    public AnimalDataBaseParameter(){}
+    // public AnimalDataBaseParameter(int AnimalID,int Level,string name,AnimalType type, List<AnimalType> slots ) { 
+    //     this.AnimalID = AnimalID;
+    //     this.Level = Level;
+    //     this.name = name;
+    //     this.type = type;
+    //     this.slots = slots;
+    //     this.newSlots = new Queue<LanguageParameter>(5);
+    // }
+    public AnimalDataBaseParameter(int AnimalID,int Level,string name,AnimalType type, int slotsCount ) { 
+        this.AnimalID = AnimalID;
+        this.Level = Level;
+        this.name = name;
+        this.type = type;
+        this.newSlots = new Queue<LanguageParameter>(slotsCount);
+        this.capacity = slotsCount;
     }
 
-    /// <summary>
-    /// sheet class name must be same with file name.
-    /// </summary>
-    [CreateAssetMenu]
-    public class AnimalDataBaseSheet : Sheet<AnimalDataBaseParameter> { }
+}
+
+/// <summary>
+/// sheet class name must be same with file name.
+/// </summary>
+[CreateAssetMenu]
+public class AnimalDataBaseSheet : Sheet<AnimalDataBaseParameter> { }
